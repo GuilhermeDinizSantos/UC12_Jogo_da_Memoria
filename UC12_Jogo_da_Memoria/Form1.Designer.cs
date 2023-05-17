8d9f21c8a799ea0511ec629ea0c9257afddae37d
GuilhermeDinizSantos <guilherme.dsantos18@GUA0636335W10-1>
2023-05-16 21:36:05 -0300

    Adicionar arquivos de projeto.

---
+++ UC12_Jogo_da_Memoria/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace UC12_Jogo_da_Memoria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelMENSAGEM = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = global::UC12_Jogo_da_Memoria.Properties.Resources.capita_marvel;
+            this.pictureBox12.Location = new System.Drawing.Point(481, 325);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox12.TabIndex = 11;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Click += new System.EventHandler(this.pictureBox12_Click);
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Image = global::UC12_Jogo_da_Memoria.Properties.Resources.capitao_america;
+            this.pictureBox11.Location = new System.Drawing.Point(325, 325);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 10;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Click += new System.EventHandler(this.pictureBox11_Click);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::UC12_Jogo_da_Memoria.Properties.Resources.feiticeira_escarlate;
+            this.pictureBox10.Location = new System.Drawing.Point(169, 325);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 9;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Click += new System.EventHandler(this.pictureBox10_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::UC12_Jogo_da_Memoria.Properties.Resources.homem_de_ferro;
+            this.pictureBox9.Location = new System.Drawing.Point(12, 325);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::UC12_Jogo_da_Memoria.Properties.Resources.loki;
+            this.pictureBox8.Location = new System.Drawing.Point(481, 169);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::UC12_Jogo_da_Memoria.Properties.Resources.capita_marvel;
+            this.pictureBox7.Location = new System.Drawing.Point(325, 169);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::UC12_Jogo_da_Memoria.Properties.Resources.capitao_america;
+            this.pictureBox6.Location = new System.Drawing.Point(169, 169);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::UC12_Jogo_da_Memoria.Properties.Resources.viuva_negra;
+            this.pictureBox5.Location = new System.Drawing.Point(12, 169);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::UC12_Jogo_da_Memoria.Properties.Resources.feiticeira_escarlate;
+            this.pictureBox4.Location = new System.Drawing.Point(481, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::UC12_Jogo_da_Memoria.Properties.Resources.homem_de_ferro;
+            this.pictureBox3.Location = new System.Drawing.Point(325, 13);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::UC12_Jogo_da_Memoria.Properties.Resources.loki;
+            this.pictureBox2.Location = new System.Drawing.Point(169, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::UC12_Jogo_da_Memoria.Properties.Resources.viuva_negra;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // labelMENSAGEM
+            // 
+            this.labelMENSAGEM.AutoSize = true;
+            this.labelMENSAGEM.Location = new System.Drawing.Point(12, 500);
+            this.labelMENSAGEM.Name = "labelMENSAGEM";
+            this.labelMENSAGEM.Size = new System.Drawing.Size(59, 13);
+            this.labelMENSAGEM.TabIndex = 12;
+            this.labelMENSAGEM.Text = "Mensagem";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 525);
+            this.Controls.Add(this.labelMENSAGEM);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.Label labelMENSAGEM;
+    }
+}
+
---
+++ UC12_Jogo_da_Memoria/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UC12_Jogo_da_Memoria
+{
+    public partial class Form1 : Form
+    {
+        string figura1 = "?";
+        string figura2 = "?";
+        string figura3 = "?";
+        string figura4 = "?";
+        string figura5 = "?";
+        string figura6 = "?";
+        string figura7 = "?";
+        string figura8 = "?";
+        string figura9 = "?";
+        string figura10 = "?";
+        string figura11 = "?";
+        string figura12 = "?";
+        int vida = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            labelMENSAGEM.Text = "";
+            pictureBox1.Image = Properties.Resources.duvida;
+            pictureBox2.Image = Properties.Resources.duvida;
+            pictureBox3.Image = Properties.Resources.duvida;
+            pictureBox4.Image = Properties.Resources.duvida;
+            pictureBox5.Image = Properties.Resources.duvida;
+            pictureBox6.Image = Properties.Resources.duvida;
+            pictureBox7.Image = Properties.Resources.duvida;
+            pictureBox8.Image = Properties.Resources.duvida;
+            pictureBox9.Image = Properties.Resources.duvida;
+            pictureBox10.Image = Properties.Resources.duvida;
+            pictureBox11.Image = Properties.Resources.duvida;
+            pictureBox12.Image = Properties.Resources.duvida;
+        }
+        public void cliques ()
+        {
+            if (vida == 2)
+            {
+                if (figura1 == "f" && figura5 != "f" || figura1 != "f" && figura5 == "f")
+                {
+                    pictureBox1.Image = Properties.Resources.duvida;
+                    pictureBox5.Image = Properties.Resources.duvida;
+                }
+                if (figura2 == "f" && figura8 != "f" || figura2 != "f" && figura8 == "f")
+                {
+                    pictureBox2.Image = Properties.Resources.duvida;
+                    pictureBox8.Image = Properties.Resources.duvida;
+                }
+                if (figura3 == "f" && figura9 != "f" || figura3 != "f" && figura9 == "f")
+                {
+                    pictureBox3.Image = Properties.Resources.duvida;
+                    pictureBox9.Image = Properties.Resources.duvida;
+                }
+                if (figura4 == "f" && figura10 != "f" || figura4 != "f" && figura10 == "f")
+                {
+                    pictureBox4.Image = Properties.Resources.duvida;
+                    pictureBox10.Image = Properties.Resources.duvida;
+                }
+                if (figura6 == "f" && figura11 != "f" || figura6 != "f" && figura11 == "f")
+                {
+                    pictureBox6.Image = Properties.Resources.duvida;
+                    pictureBox11.Image = Properties.Resources.duvida;
+                }
+                if (figura7 == "f" && figura12 != "f" || figura7 != "f" && figura12 == "f")
+                {
+                    pictureBox7.Image = Properties.Resources.duvida;
+                    pictureBox12.Image = Properties.Resources.duvida;
+                }
+                if (figura1 == "f" && figura5 != "f" || figura1 != "f" && figura5 == "f")
+                {
+                    pictureBox1.Image = Properties.Resources.duvida;
+                    pictureBox5.Image = Properties.Resources.duvida;
+                }
+                figura1 = "?";
+                figura2 = "?";
+                figura3 = "?";
+                figura4 = "?";
+                figura5 = "?";
+                figura6 = "?";
+                figura7 = "?";
+                figura8 = "?";
+                figura9 = "?";
+                figura10 = "?";
+                figura11 = "?";
+                figura12 = "?";
+                vida = 0;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (figura1 == "?")
+            {
+                pictureBox1.Image = Properties.Resources.viuva_negra;
+                figura1 = "f";
+            } else
+            {
+                pictureBox1.Image = Properties.Resources.duvida;
+                figura1 = "?";
+            }
+            if (figura1 == "f" && figura5 == "f")
+            {
+                pictureBox1.Enabled = false;
+                pictureBox5.Enabled = false;
+            }
+            vida++;
+            cliques();
+            ganhou();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (figura2 == "?")
+            {
+                pictureBox2.Image = Properties.Resources.loki;
+                figura2 = "f";
+            }
+            else
+            {
+                pictureBox2.Image = Properties.Resources.duvida;
+                figura2 = "?";
+            }
+            if (figura2 == "f" && figura8 == "f")
+            {
+                pictureBox2.Enabled = false;
+                pictureBox8.Enabled = false;
+            }
+            vida++;
+            cliques();
+            ganhou();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (figura3 == "?")
+            {
+                pictureBox3.Image = Properties.Resources.homem_de_ferro;
+                figura3 = "f";
+            }
+            else
+            {
+                pictureBox3.Image = Properties.Resources.duvida;
+                figura3 = "?";
+            }
+            if (figura3 == "f" && figura9 == "f")
+            {
+                pictureBox3.Enabled = false;
+                pictureBox9.Enabled = false;
+            }
+            vida++;
+            cliques();
+            ganhou();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (figura4 == "?")
+            {
+                pictureBox4.Image = Properties.Resources.feiticeira_escarlate;
+                figura4 = "f";
+            }
+            else
+            {
+                pictureBox4.Image = Properties.Resources.duvida;
+                figura4 = "?";
+            }
+            if (figura4 == "f" && figura10 == "f")
+            {
+                pictureBox4.Enabled = false;
+                pictureBox10.Enabled = false;
+            }
+            vida++;
+            cliques();
+            ganhou();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (figura5 == "?")
+            {
+                pictureBox5.Image = Properties.Resources.viuva_negra;
+                figura5 = "f";
+            }
+            else
+            {
+                pictureBox5.Image = Properties.Resources.duvida;
+                figura5 = "?";
+            }
+            if (figura1 == "f" && figura5 == "f")
+            {
+                pictureBox1.Enabled = false;
+                pictureBox5.Enabled = false;
+            }
+            vida++;
+            cliques();
+            ganhou();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            if (figura6 == "?")
+            {
+                pictureBox6.Image = Properties.Resources.capitao_america;
+                figura6 = "f";
+            }
+            else
+            {
+                pictureBox6.Image = Properties.Resources.duvida;
+                figura6 = "?";
+            }
+            if (figura6 == "f" && figura11 == "f")
+            {
+                pictureBox6.Enabled = false;
+                pictureBox11.Enabled = false;
+            }
+            vida++;
+            cliques();
+            ganhou();
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            if (figura7 == "?")
+            {
+                pictureBox7.Image = Properties.Resources.capita_marvel;
+                figura7 = "f";
+            }
+            else
+            {
+                pictureBox7.Image = Properties.Resources.duvida;
+                figura7 = "?";
+            }
+            if (figura7 == "f" && figura12 == "f")
+            {
+                pictureBox7.Enabled = false;
+                pictureBox12.Enabled = false;
+            }
+            vida++;
+            cliques();
+            ganhou();
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            if (figura8 == "?")
+            {
+                pictureBox8.Image = Properties.Resources.loki;
+                figura8 = "f";
+            }
+            else
+            {
+                pictureBox8.Image = Properties.Resources.duvida;
+                figura8 = "?";
+            }
+            if (figura2 == "f" && figura8 == "f")
+            {
+                pictureBox2.Enabled = false;
+                pictureBox8.Enabled = false;
+            }
+            vida++;
+            cliques();
+            ganhou();
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            if (figura9 == "?")
+            {
+                pictureBox9.Image = Properties.Resources.homem_de_ferro;
+                figura9 = "f";
+            }
+            else
+            {
+                pictureBox9.Image = Properties.Resources.duvida;
+                figura9 = "?";
+            }
+            if (figura3 == "f" && figura9 == "f")
+            {
+                pictureBox3.Enabled = false;
+                pictureBox9.Enabled = false;
+            }
+            vida++;
+            cliques();
+            ganhou();
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+            if (figura10 == "?")
+            {
+                pictureBox10.Image = Properties.Resources.feiticeira_escarlate;
+                figura10 = "f";
+            }
+            else
+            {
+                pictureBox10.Image = Properties.Resources.duvida;
+                figura10 = "?";
+            }
+            if (figura4 == "f" && figura10 == "f")
+            {
+                pictureBox4.Enabled = false;
+                pictureBox10.Enabled = false;
+            }
+            vida++;
+            cliques();
+            ganhou();
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+            if (figura11 == "?")
+            {
+                pictureBox11.Image = Properties.Resources.capitao_america;
+                figura11 = "f";
+            }
+            else
+            {
+                pictureBox11.Image = Properties.Resources.duvida;
+                figura11 = "?";
+            }
+            if (figura6 == "f" && figura11 == "f")
+            {
+                pictureBox6.Enabled = false;
+                pictureBox11.Enabled = false;
+            }
+            vida++;
+            cliques();
+            ganhou();
+        }
+
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+            if (figura12 == "?")
+            {
+                pictureBox12.Image = Properties.Resources.capita_marvel;
+                figura12 = "f";
+            }
+            else
+            {
+                pictureBox12.Image = Properties.Resources.duvida;
+                figura12 = "?";
+            }
+            if (figura7 == "f" && figura12 == "f")
+            {
+                pictureBox7.Enabled = false;
+                pictureBox12.Enabled = false;
+            }
+            vida++;
+            cliques();
+            ganhou();
+        }
+        public void ganhou()
+        {
+            if (figura1 == "f" && figura2 == "f" && figura3 == "f" && figura4 == "f" && figura5 == "f" && figura6 == "f" && figura7 == "f" && figura8 == "f" && figura9 == "f" && figura10 == "f" && figura11 == "f" && figura12 == "f")
+            {
+                labelMENSAGEM.Text = "Você conseguiu, parabéns!";
+                labelMENSAGEM.BackColor = Color.Green;
+            }
+        }
+    }
+}
